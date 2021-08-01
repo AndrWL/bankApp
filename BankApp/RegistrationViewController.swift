986e9008345a95dd7bcb4b8f5815c7f4e1ad09e5
andrew <64851858+AndrWL@users.noreply.github.com>
2021-08-01 10:47:47 +0300

    Use MVC

---
+++ BankApp/RegistrationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationViewController.swift
+//  BankApp
+//
+//  Created by Andrey on 01.08.2021.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        
+    }
+    
+
+
+
+}
