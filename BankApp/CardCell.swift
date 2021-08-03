d22869e6df8652e6e4053ae32587a6cc08636fd9
andrew <64851858+AndrWL@users.noreply.github.com>
2021-08-03 22:12:08 +0300

    test collection layout

---
+++ BankApp/CardCell.swift
@@ -0,0 +1,15 @@
+//
+//  CardCell.swift
+//  BankApp
+//
+//  Created by Andrey on 03.08.2021.
+//
+
+import UIKit
+
+
+class CardCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardImage: UIImageView!
+    
+}
---
+++ BankApp/CardModel.swift
@@ -0,0 +1,14 @@
+//
+//  CardModel.swift
+//  BankApp
+//
+//  Created by Andrey on 03.08.2021.
+//
+
+import UIKit
+
+struct CardModel {
+    
+    let id: Int?
+    let cardImage: UIImage?
+}
