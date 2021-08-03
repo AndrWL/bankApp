2fdd70300f647a0c1341a31dcbace95fcf5472a5
andrew <64851858+AndrWL@users.noreply.github.com>
2021-08-03 11:47:30 +0300

    creat segue

--- BankApp/RegistrationViewController.swift
+++ BankApp/RegistrationViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
+    
 
+    @IBOutlet weak var userNameTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +21,28 @@ class RegistrationViewController: UIViewController {
     }
     
 
+    @IBAction func userNameAction(_ sender: UITextField) {
+        guard userNameTextfield.text?.isEmpty == false else {
+            return
+        }
+        if let _ = Double(userNameTextfield.text!) {
+            let alert = UIAlertController(title: "Warning", message: "UserName must use character", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Ok", style: .default)
+                alert.addAction(okAction)
+            present(alert, animated: true)
+        }
+        
+    }
+    
+    @IBAction func passwordAction(_ sender: UITextField) {
+        
+    }
+    
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToMain" {
+            print("yes")
+        }
+    }
 
 }
